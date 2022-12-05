861cc5317bfbf83be49d2ce298ed0b722129be5d
samson adeyemi <samadeyemi1888@gmail.com>
2022-12-05 11:08:57 -0700

    learning stages

--- followed_project_buider.flutter/dart_online_package/lib/dart_online_package.dart
+++ followed_project_buider.flutter/dart_online_package/lib/dart_online_package.dart
@@ -1,3 +1,31 @@
+import 'dart:math';
+
 void main() {
-  print("the boy is bad");
+  final person = Person(name: 'samson', age: 64, height: 1.84);
+  print(person.age);
+  print(person.Descibe2('keder', 45));
+  print(person.myName('shola'));
+  final employee = Employee('samson ', 46, 1.56, taxCode: 'Abi7', salary: 5000);
+  print(employee.taxCode);
+  print(employee.myName('lakaka'));
+}
+
+class Person {
+  Person({required this.name, required this.age, required this.height});
+  final String name;
+  final int age;
+  final double height;
+  // function is called method  inside a class
+  String Descibe2(String name, int age, [double height = 0]) =>
+      "my name is $name, i am $age years old, iam $height meters tall";
+  String myName(String name) => 'my age is $name';
+  void sayAge(int age) => print('my age is $age');
+}
+
+class Employee extends Person {
+  Employee(String name, int age, double height,
+      {required this.taxCode, required this.salary})
+      : super(name: name, age: age, height: height);
+  final String taxCode;
+  final int salary;
 }
